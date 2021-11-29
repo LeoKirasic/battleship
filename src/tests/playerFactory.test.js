bf46fef3dbdff50b5cc9bdb0c164135b7b1f9f8b
LeoKirasic <kirasicleo@gmail.com>
2021-11-29 18:22:28 +0100

    Add tests for isSunk function

--- src/tests/playerFactory.test.js
+++ src/tests/playerFactory.test.js
@@ -26,3 +26,21 @@ test('hit function pushes values in correct order', () => {
 
   expect(ship.hitArray).toEqual(expectedArray);
 });
+
+test('isSunk positive test', () => {
+  const ship = shipFactory(3);
+  ship.hit(0);
+  ship.hit(1);
+  ship.hit(2);
+
+  expect(ship.isSunk(3)).toBe(true);
+});
+
+test('isSunk negative test', () => {
+  const ship = shipFactory(4);
+  ship.hit(0);
+  ship.hit(1);
+  ship.hit(2);
+
+  expect(ship.isSunk(4)).toBe(false);
+});

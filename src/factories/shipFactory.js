56661928d7a3ca0f93883fe94583f6f8dc5677d6
LeoKirasic <kirasicleo@gmail.com>
2021-12-02 20:05:37 +0100

    re-write ship placement to include length

--- src/factories/shipFactory.js
+++ src/factories/shipFactory.js
@@ -1,18 +1,41 @@
 const shipFactory = (length, coordinates) => {
   const getLength = () => length;
   const getCoordinates = () => coordinates;
-  const hitCoordinatesArray = [];
-  const isHit = (coordinate) => {
-    hitCoordinatesArray.push(coordinate);
+
+  const longCoords = [];
+  const place = (length, coordinates) => {
+    let counter = 0;
+    for (let i = 0; i < length; i++) {
+      counter = coordinates[1] + i;
+      longCoords.push([coordinates[0], counter]);
+      counter++;
+    }
   };
 
-  const isSunk = (length) => {
-    if (length === hitCoordinatesArray.length) {
+  const hitArray = [];
+  const coords = coordinates;
+  let hitCounter = 0;
+  const hit = () => {
+    hitArray.push(hitCounter);
+    hitCounter++;
+  };
+
+  const isSunk = () => {
+    if (getLength() === hitArray.length) {
       return true;
     }
     return false;
   };
-  return { isHit, isSunk };
+  return {
+    hit,
+    isSunk,
+    hitArray,
+    getLength,
+    getCoordinates,
+    coords,
+    place,
+    longCoords,
+  };
 };
 
 export { shipFactory };
